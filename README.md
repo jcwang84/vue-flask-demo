00f0d23b4fce9e3c0e51cd7872de9bf829d833b0
unknown <jcwang1984@gmail.com>
2020-08-10 20:08:48 +0100

    add login without DB

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 #vue-flask-demoo
 
 a backend service for vuejs
+
+# [0.0.1] -- 10/08/2020
+## Add Login page with css and html no DB connection yet
--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 from flask_cors import CORS
 
 DEBUG = True
@@ -60,6 +60,21 @@ def get_books():
 
     return jsonify(response_object)
 
+@app.route('/home')
+def home():
+    return 'Login success!'
+
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run()
